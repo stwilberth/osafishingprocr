d9988b239163066e68acfd5300c68da349361230
Wilberth Loría <stwilberth@gmail.com>
2025-05-13 20:15:12 -0600

    e

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -23,12 +23,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     @yield('styles')
 
-    @if (app()->environment('production'))
-        <script src="{{ asset('build/assets/app1_1.js') }}" type="module" defer></script>
-        <link href="{{ asset('build/assets/app1_1.css') }}" rel="stylesheet">
-    @else
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @endif
+    <!-- production -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans text-gray-900 antialiased">
